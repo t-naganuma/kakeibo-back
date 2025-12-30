9774b3fe73897911fd4940f3cea0ef468189124e
t-naganuma <takuya.naganuma@gmail.com>
2025-12-31 08:02:03 +0900

    first commit

---
+++ test/kakeibo-back.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as KakeiboBack from '../lib/kakeibo-back-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/kakeibo-back-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new KakeiboBack.KakeiboBackStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
